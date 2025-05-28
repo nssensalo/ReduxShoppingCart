df68a9a88c85ae0bbcf63d84ef9183be92b51246
Nansamba Ssensalo <nssensalo@gmail.com>
2025-05-28 16:51:12 -0700

    adding decrementQuantity reducer

--- src/state/cartSlice.js
+++ src/state/cartSlice.js
@@ -9,7 +9,7 @@ const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        addToCart:(state, payload) => {
+        addToCart:(state, action) => {
             const { id, name, price, quantity} = action.payload; //properties on items[] objects
             const existingItem = state.items.find(item => item.id === id); 
             //iterated over existing items object for one who's id matches 
@@ -23,19 +23,29 @@ const cartSlice = createSlice({
             state.totalPrice = state.items.reduce((total,item)=>total + item.price * state.quantity,0)
         },
         removeFromCart: (state) => {
-           state.items =  state.items.filter(item => !item === id)
+           state.items =  state.items.filter(item => item !== id)
             state.totalPrice = state.items.reduce((total,item)=>total + item.price * item.quantity,0)
         },
 
-        reduceItemInCart: (state) => {
-            if (st)
+        decrementQuantity: (state, action) => {
+           const {id} = action.payload
+           const existingItem = state.items.find(item => item.id === id )
+           
+           if (existingItem) {
+                if (existingItem.quantity > 1 ) {
+                    existingItem.quantity -= 1
+                }
+           } else {
+            state.items = state.items.filter(item => item.id !== item)
+           }
+           
+          state.totalPrice = state.items.reduce((total, item)=>total + item.price * item.quantity,0)
         }
-       
     }
         
 
-})
- export const { addToCart,removeFromCart} = createSlice.actions;
+});
+ export const { addToCart,removeFromCart,decrementQuantity} = createSlice.actions;
  export default cartSlice.reducers;
 
 
