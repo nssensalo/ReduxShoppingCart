9fb80dbb9a5fefededb90096504c3ac508f3b62c
Nansamba Ssensalo <nssensalo@gmail.com>
2025-05-28 17:27:21 -0700

    Adding clearCart and setQuanitity reducers

--- src/state/cartSlice.js
+++ src/state/cartSlice.js
@@ -31,15 +31,44 @@ const cartSlice = createSlice({
            const {id} = action.payload
            const existingItem = state.items.find(item => item.id === id )
            
-           if (existingItem) {
+           if (existingItem) { //defensive against race conditions
                 if (existingItem.quantity > 1 ) {
                     existingItem.quantity -= 1
                 }
-           } else {
+           } else { //else if would be wrong and always false ,1 vs 0
             state.items = state.items.filter(item => item.id !== item)
            }
-           
-          state.totalPrice = state.items.reduce((total, item)=>total + item.price * item.quantity,0)
+           state.totalPrice = state.items.reduce((total, item)=>total + item.price * item.quantity,0)
+        },
+        incrementQuantity: ( state, action) => {
+            const {id} = action.payload //quantity is accessed through the state itself
+            const existingItem = state.items.find(item => item.id === id)
+
+            if(existingItem) {
+               existingItem.quantity += 1
+            }
+            state.totalPrice = state.items.reduce((total, item)=>total + item.price * item.quantity,0)
+        },
+        clearCart: (state) => {
+            state.items = []
+            state.totalPrice = 0 //dont need to deal with zeroing out quantity on cleared cart
+        },
+        setItemQuantity: (state, action) => {
+            const {id,newQuantity,name,price} = action.payload
+            const existingItem = state.items.find(item=> item.id ===id)
+            if (existingItem) {
+                if(newQuantity > 0) {
+                    existingItem.quantity = newQuantity
+                } else if (newQuantity) { //its 0, null, undefined
+                    state.items = state.items.filter(item => item.id !== id)
+                }
+            } else if (!existingItem && newQuantity) {
+                state.items.push({id, name, price, newQuantity})
+            }
+            state.totalPrice = state.items.reduce((total, item)=>total + item.price * item.quantity,0)
+            // Ensure newQuantity is treated as a valid non-negative number.
+            // This handles cases where user input might be non-numeric or negative.
+            //const parsedNewQuantity = Math.max(0, Number(newQuantity) || 0);
         }
     }
         
